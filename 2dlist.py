a387f52352fb7fb4b16b9a98bb9bc6f86e22cc81
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-20 12:11:41 +0300

    Add files via upload

---
+++ 2dlist.py
@@ -0,0 +1,25 @@
+# 2D LISTS ARE EXAMPLE OF MATRICES
+
+matrix = [
+    [1, 2, 3],
+    [3, 6, 8],
+    [6, 7, 9]
+]
+
+matrix[0] # INDEX 0 OF THE MATRIX REPRESENTS LISTS AVAILABLE IN THE LIST
+print(matrix)
+print(matrix[0] [1]) # ACCESSING VALUE IN THE INNER LIST
+(matrix[0] [1])
+print(matrix)
+
+# ITERATING OVER A 2D LIST
+
+
+matrix = [
+    [1, 2, 3],
+    [3, 6, 8],
+    [6, 7, 9]
+]
+for row in matrix:
+    for items in row:
+        print(items)
