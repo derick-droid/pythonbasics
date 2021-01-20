98c1a21f2a1811dc5241ba7b039d0e1d2791782b
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-20 10:04:14 +0300

    Add files via upload
    practice program for if statements

---
+++ ifstatement.py
@@ -0,0 +1,26 @@
+is_hot = False
+is_cold = False
+if is_hot:
+    print("it is a hot day")
+    print("drink a plenty of water")
+
+elif is_cold:
+    print("it is a cold day ")
+    print("wear warm clothes")
+else:
+    print("it is a lovely day")
+
+print("enjoy your day")
+
+
+# IF STATEMENTS EXERCISE
+mark_price = 1000000
+good_credit = False
+if good_credit:
+    down_payment = mark_price * 0.1
+    print(f"price:  ${down_payment}")
+else:
+    down_payment = mark_price * 0.2
+    print(f"price: ${down_payment}")
+
+print("thank you for purchasing with us")
