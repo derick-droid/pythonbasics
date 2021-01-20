756c2755ca99d1afea4f23dda94a21d1bc8000aa
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-20 10:06:10 +0300

    Add files via upload
    practice program for maths functions

---
+++ mathsfunctions.py
@@ -0,0 +1,20 @@
+# round off
+x = 2.899
+print(round(x))
+y = 9000.84474
+print(round(y))
+m = 999999837
+
+print(round(m))
+# ABSOLUTE FUNCTION
+Z = -8887489
+print(abs(Z))
+r = -535
+print(abs(r))
+
+# IMPORTING BUILT IN MATHS FUNCTIONS IN PYTHON
+
+import math
+
+print(math.floor(67))
+print(math.acosh(5))
