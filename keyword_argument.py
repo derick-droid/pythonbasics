66d51522c81a6dd75ff8dd5530fdf92fd995deb8
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-27 14:33:09 +0300

    Add files via upload

---
+++ keyword_argument.py
@@ -0,0 +1,10 @@
+def welcoming_greet(first_name, last_name):
+    print(f"hi {first_name}  {last_name} ")
+    print("welcome abroad ")
+
+
+print("hey new employee")
+welcoming_greet(first_name="john", last_name="monly") # keyword argument
+welcoming_greet("derrick", "okinda") # positional argument/ positional argument do come first before keyword argument
+print("thanking fo accepting our call")
+
