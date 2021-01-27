75a1ae9a0488c387bbae0ea1ce8066b1df5e15a2
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-27 14:57:49 +0300

    Add files via upload
    basic reusable functions

---
+++ reusable_function.py
@@ -0,0 +1,8 @@
+# USABLE FUNCTION HELPS TO AVOID CODING MORE CODES
+def welcoming_greet(name):
+    print(f"hi {name} there")
+
+
+name = input("what is your name:  ")
+print(name)
+welcoming_greet(name)
