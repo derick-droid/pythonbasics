3261da90aedda65154428db2108f186e55eb8edc
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-27 14:10:33 +0300

    Add files via upload

---
+++ parameter.py
@@ -0,0 +1,10 @@
+def welcoming_greet(first_name, last_name):
+    print(f"hi {first_name}  {last_name} ")
+    print("welcome abroad ")
+
+
+print("hey new employee")
+welcoming_greet("john", "monly")
+welcoming_greet("derrick", "okinda")
+print("thanking fo accepting our call")
+
