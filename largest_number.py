a1de9764cfdbda03d0dfbd3f4d59dfd67574af69
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-28 11:05:43 +0300

    Add files via upload

---
+++ largest_number.py
@@ -0,0 +1,17 @@
+# def largest_number(numbers):
+#     biggest_number = numbers[0]
+#     for number in numbers:
+#         if number > biggest_number:
+#             biggest_number = number
+#             return biggest_number
+# numbers = [2, 3, 73, 83, 27, 7, ]
+def find_biggest_number(numbers):
+    biggest_number = numbers[0]
+    for number in numbers:
+        if number > biggest_number:
+            biggest_number = number
+    return biggest_number
+
+
+
+
