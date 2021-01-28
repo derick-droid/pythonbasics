1341bb4b7f0774232c9640c4332c8896956ff7c1
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-28 11:05:25 +0300

    Add files via upload

---
+++ module.py
@@ -0,0 +1,9 @@
+# from largest_number import largest_number
+# biggest_number = largest_number(numbers)
+# print(biggest_number)
+from largest_number import find_biggest_number
+numbers = [2, 3, 73, 83, 27, 7, ]
+biggest_number = find_biggest_number(numbers)
+print(biggest_number)
+
+
