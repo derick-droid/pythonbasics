bc6dc2c60c2f08e39721524b79fc829c4ee0f92c
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-01-31 18:03:07 +0300

    Add files via upload
    files and directory simple notes 
    how to check files and directories in a project

---
+++ files and directories.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+# Absolute path
+# relative path
+path = Path("ecommerce")
+print(path.exists())  # exists method checks the presence of a directory
+# print(path.mkdir())  # to make a directory
+# print(path.rmdir())  # this is to make a directory
+# glob method ro search files in a directory
