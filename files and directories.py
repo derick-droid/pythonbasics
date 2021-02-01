c2bf1e1e75071d3317149855854d2aef7593a468
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-02 00:13:35 +0300

    Add files via upload

--- files and directories.py
+++ files and directories.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+path = Path()
+for file in path.glob("*.py"):
+    print(file)
 # Absolute path
 # relative path
 path = Path("ecommerce")
 print(path.exists())  # exists method checks the presence of a directory
 # print(path.mkdir())  # to make a directory
-# print(path.rmdir())  # this is to make a directory
+# print(path.rmdir())  # this is to remove a directory
 # glob method ro search files in a directory
+
