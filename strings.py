e6bd3d85f3fe3fae984ed6b00a6e975f8fdb76b1
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-03 14:34:07 +0300

    Add files via upload
    basic concepts in string

---
+++ strings.py
@@ -0,0 +1,20 @@
+print("GIRAFFE")
+print("Giraffe\nAcademy")  # the \n is used to create new tab between the strings
+print("Giraffe\"academy")  # the escalation inserts quotation in between the words
+phrase = "giraffe academy"  # storing string in a variable
+print(phrase)
+# contenation of a string
+
+print(phrase + " is cool")
+
+#  using the string methods
+print(phrase.lower())  # converting to lower case
+print(phrase.upper())  # converting to upper case
+print(phrase.isupper())  # is used to check if string is upper case
+print(phrase.islower())  # is used to check if string is in lower case
+print(phrase.upper().isupper())  # thi is also applicable
+print(len(phrase))  # the len function shows total number characters present in a string
+print(phrase[0])  # accessing the individual characters from a string
+print(phrase.index("a"))  # the index function helps to allocate the index of a character in a string
+print(phrase.replace("giraffe", "lion"))  # the replace function is used to replace an existing character in a string with another
+ 
