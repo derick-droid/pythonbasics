e50cde29d8a6f68edab71489ac61dbe8d1493311
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-03 15:12:43 +0300

    Add files via upload
    simple math python code

---
+++ numbers.py
@@ -0,0 +1,21 @@
+print(2+3)  # the addition operation in python
+print(4_4)  # the subtraction operation in python
+print(3*6)  # the multiplication operation in python
+print(10 % 3)  # the modulus operation which gives the remainder as answer
+print(10 / 3)  # this is a divisional operation
+print(10//3)  # this gives a whole answer and ignores the remainder
+print(3**2) # this shows the power
+
+# THE METHODS USED
+print(abs(-10))  # this brings absolute value of a number
+print(pow(3, 2))  # this also gives power of a value
+print(max(4, 6))  # this shows the maximum number
+print(min(4, 6))  # this shows the minimum number
+print(round(3.22))  # this rounds off the value of a number
+
+# importing other maths function
+
+from math import *
+print(floor(3.7))  # the floor method prints lowest value and ignores the decimal places
+print(ceil(3.2))  # this will round off the number whatever the values are available
+print(sqrt(36))  # this will the square root of an umber
