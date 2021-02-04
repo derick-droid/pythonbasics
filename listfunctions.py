aa95f6d4c7a87c10ae94bc2e179ed81752ffcc09
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-04 11:04:09 +0300

    Add files via upload
    simple basic functions in lists

---
+++ listfunctions.py
@@ -0,0 +1,6 @@
+lucky_numbers = [1, 2, 3, 4, 5, 6, 7]
+friends = ["derrick", "jim", "jim", "trump", "majani" ]
+friends.extend(lucky_numbers)  # to add another list on another list
+print(friends)
+print(friends.count("jim"))  # to count repetitive objects in a list
+
