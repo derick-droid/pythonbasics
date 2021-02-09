11f2b5e5d639a4ab1f021ab5bd193c67c1633255
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-09 18:57:52 +0300

    Add files via upload
    simple dictionary workout

---
+++ dictionarycsdojo.py
@@ -0,0 +1,15 @@
+naming = {
+    "geogre": 24,
+    "derrick": 23,
+    "enock": 34,
+    "jehova": 23
+
+
+}
+
+for keys, values in naming.items():
+    print("keys")
+    print(keys)
+    print("values")
+    print(values)
+    print(" ")
