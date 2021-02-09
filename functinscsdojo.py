b89f7f8e97341a07317f39d6e1dbc3db8d02c7ff
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-09 18:56:23 +0300

    Add files via upload
    sim'le function code

---
+++ functinscsdojo.py
@@ -0,0 +1,22 @@
+# functions as collection of codes
+def function1():
+    print("this is s function ")
+    print("thi is still within the function")
+
+
+print("this is outside the function")
+
+# using function as a mapping
+
+
+def function2(x, y):
+    return x + y
+
+
+print("here we start ")
+function1()
+
+print(function2(3, 6))
+
+
+
