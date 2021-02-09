f5c9fdfef8b2516804526798ba127abbb078470a
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-09 22:40:26 +0300

    Add files via upload
    more on basic if statements

---
+++ ifstatementcsdojo.py
@@ -0,0 +1,29 @@
+a = 1
+b = 2
+if a < b:
+    print("a is less than b")
+
+else:
+    print("a is greater than b")
+
+# more of if statements
+
+f = 18
+j = 4
+if f > j + 10:
+    print("f is greater than j by 10")
+elif j > f +10:
+    print("j is greater than f by 10")
+else:
+    print("none of the above comparison")
+
+# more of if statement
+h = 18
+z = 23
+if h > z:
+    print("h is greater than z ")
+else:
+    if h == z:
+        print("the two variables are equal to each other")
+    else:
+        print("h is less than z")
