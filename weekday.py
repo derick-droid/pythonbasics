d40ef5f158c623506e24ab4e19f27758a156334f
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-20 11:40:58 +0300

    Add files via upload
    exercise in coding bat

---
+++ weekday.py
@@ -0,0 +1,8 @@
+def sleep_in(weekday, vacation):
+    if weekday == True or vacation == False:
+        print(True)
+        # return True
+    elif weekday == False or vacation == True:
+        return  True
+    else:
+        return False
