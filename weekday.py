730df2fcc9cb86f6e1d5423387ab4c9cdb2f471a
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-02-25 14:38:12 +0300

    Add files via upload
    solutions for question from coding bat site

--- weekday.py
+++ weekday.py
@@ -1,8 +1,11 @@
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+#
+# sleep_in(False, False) → True
+# sleep_in(True, False) → False
+# sleep_in(False, True) → True
+
 def sleep_in(weekday, vacation):
-    if weekday == True or vacation == False:
-        print(True)
-        # return True
-    elif weekday == False or vacation == True:
-        return  True
+    if weekday or vacation:
+        return True
     else:
         return False
