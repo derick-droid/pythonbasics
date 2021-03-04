2b4b2e85ff24b2d755e0b7c832c4b1ec6396b83f
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-03-04 13:16:41 +0300

    Add files via upload

---
+++ forlooptechnique.py
@@ -0,0 +1,17 @@
+# printing alternating D and C
+print("A")
+print("B")
+for i in range(5):
+    print("C")
+    print("D")
+
+print("out of the loop")
+# printing five c firsst the d follows
+
+print("A")
+print("B")
+for i in range(5):
+    print("C")
+for i in range(5):
+    print("D")
+print("out of the loop")
