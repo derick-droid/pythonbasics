49e3a0e86acde7b214494e3e7d2535b549917d61
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-06-07 15:44:06 +0300

    Add files via upload
    positional argument in functions

---
+++ arguments.py
@@ -0,0 +1,9 @@
+# positional argument
+def my_pet (animal_type, animal_name):
+    print(f"\nI have {animal_type} ")
+    print(f"{animal_type} name is {animal_name} ")
+    
+    
+my_pet("cat", "pusss")
+# calling multiple functions
+my_pet("dog", "rex")
