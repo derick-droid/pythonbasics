7536cfe8bc26251e3e35e9f2dae7b890475f1f0f
derick-droid <66371514+derick-droid@users.noreply.github.com>
2021-06-11 11:19:14 +0300

    Add files via upload
    optional arguments in python

---
+++ optional.py
@@ -0,0 +1,12 @@
+# optional arguments
+def user_names(first_name, last_name, middle_name = ""):
+    if middle_name:
+        full_name = first_name + " " + middle_name + " " + last_name + "."
+        return full_name
+    else:
+   
+        full_name = first_name + " " + last_name + "."
+        return full_name
+        
+musician = user_names("Omari", "hardwick", "jj")
+print(musician)
